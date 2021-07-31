70566816049937de27f1602e4b1160b1906a08e3
KanmiGarasu <k_ketsu@outlook.com>
2021-07-31 09:23:39 +0800

    stringBuilderTest "字符串拼接底层优化以及StringBuilder笔记"

--- Day12_file_bytestream/src/com/itheima/fileTest/stringBuilderTest.java
+++ Day12_file_bytestream/src/com/itheima/fileTest/stringBuilderTest.java
@@ -7,7 +7,7 @@ public class stringBuilderTest {
         String s1 = "a";
         String s2 = "b";
         String s3 = "c";
-        //常量拼接底层存在自动优化的特点
+        //常量拼接底层存在自动优化的特点(不建议
         String s4 = s1+s2+s3;
         //"ab"+"c"+"abc"会产生用不到的中间字符串
         //如果字符串拼接次数多，不能使用String
@@ -19,3 +19,13 @@ public class stringBuilderTest {
         String newStr = sb.toString();
     }
 }
+/**
+ 总结：
+    1.如果拼接的字符串都是常量，符合常量优化机制
+    2.如果拼接字符串有变量存在：
+        String S4 = S1+S2+S3;
+        new StringBuilder.append(S1)....append(S3)..
+    3.最终方案：
+        自己创建StringBuilder对象，自己拼接
+
+ */
